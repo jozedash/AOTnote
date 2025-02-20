43485b525b8ae8af3fd54446cb2e38d201b94871
jozedash <josiah.kemp@virginmedia.com>
2025-02-20 22:08:42 +0800

    #3 Added AlwaysOnTop parameter to config, may want to make this a setting in future...

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 250,
+    height: 250,
+    alwaysOnTop: true
   })
 
   win.loadFile('index.html')
